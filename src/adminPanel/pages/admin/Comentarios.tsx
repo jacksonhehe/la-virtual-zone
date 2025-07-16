c21bd409c7c553f74690487f54d91f34cb1e7db5
jacksonhehe <jackcano14@gmail.com>
2025-07-16 10:21:47 -0500

    Add src/adminPanel/pages/admin/Comentarios.tsx 1752679307780

---
+++ src/adminPanel/pages/admin/Comentarios.tsx
@@ -0,0 +1,12 @@
+import  CommentsAdminPanel from '../../components/admin/CommentsAdminPanel';
+
+const Comentarios = () => {
+  return (
+    <div className="p-6">
+      <CommentsAdminPanel /> 
+    </div>
+  );
+};
+
+export default Comentarios; 
+ 
