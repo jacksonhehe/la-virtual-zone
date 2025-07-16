628aa10bd1548442e63aa719577b044e3e35c8b4
jacksonhehe <jackcano14@gmail.com>
2025-07-16 10:19:18 -0500

    Add src/utils/slugify.ts 1752679158170

---
+++ src/utils/slugify.ts
@@ -0,0 +1,14 @@
+export const slugify = (text: string): string => {
+  return text
+    .toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+};
+
+export default slugify;
