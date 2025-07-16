5f4c26e668a0fa342fb90fca05ff3f8b11c13a2a
jacksonhehe <jackcano14@gmail.com>
2025-07-16 10:21:49 -0500

    Add src/adminPanel/pages/admin/Calendario.tsx 1752679309800

---
+++ src/adminPanel/pages/admin/Calendario.tsx
@@ -0,0 +1,13 @@
+import  CalendarAdminPanel from '../../components/admin/CalendarAdminPanel';
+
+const Calendario = () => {
+  return (
+    <div className="p-6">
+      <CalendarAdminPanel /> 
+    </div>
+  );
+};
+
+export default Calendario;
+ 
+ 
