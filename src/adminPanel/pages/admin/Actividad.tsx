374f987dd501b79b4fedee0a7210b6270eb5bf89
jacksonhehe <jackcano14@gmail.com>
2025-07-16 10:21:50 -0500

    Add src/adminPanel/pages/admin/Actividad.tsx 1752679310907

---
+++ src/adminPanel/pages/admin/Actividad.tsx
@@ -0,0 +1,12 @@
+import  ActivityAdminPanel from '../../components/admin/ActivityAdminPanel';
+
+const Actividad = () => {
+  return (
+    <div className="p-6">
+      <ActivityAdminPanel /> 
+    </div>
+  );
+};
+
+export default Actividad;
+ 
