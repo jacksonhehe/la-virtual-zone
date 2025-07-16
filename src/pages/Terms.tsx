d0aff533986f0742842efa8bb1041df5538b5684
jacksonhehe <jackcano14@gmail.com>
2025-07-16 10:19:41 -0500

    Add src/pages/Terms.tsx 1752679182479

---
+++ src/pages/Terms.tsx
@@ -0,0 +1,9 @@
+import TermsContent from '../../docs/legal/terms.md';
+
+const Terms = () => (
+  <div className="prose prose-invert max-w-3xl mx-auto p-4">
+    <TermsContent />
+  </div>
+);
+
+export default Terms;
