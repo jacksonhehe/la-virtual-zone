1e64b762da8a18b087323a0cda874d7096d5be12
jacksonhehe <jackcano14@gmail.com>
2025-07-16 10:19:48 -0500

    Add src/pages/Privacy.tsx 1752679189451

---
+++ src/pages/Privacy.tsx
@@ -0,0 +1,9 @@
+import PrivacyContent from '../../docs/legal/privacy.md';
+
+const Privacy = () => (
+  <div className="prose prose-invert max-w-3xl mx-auto p-4">
+    <PrivacyContent />
+  </div>
+);
+
+export default Privacy;
