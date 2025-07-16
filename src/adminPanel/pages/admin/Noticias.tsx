b623725e636067d06cb7542b94b672ef99d2316d
jacksonhehe <jackcano14@gmail.com>
2025-07-16 10:21:41 -0500

    Add src/adminPanel/pages/admin/Noticias.tsx 1752679302785

---
+++ src/adminPanel/pages/admin/Noticias.tsx
@@ -0,0 +1,12 @@
+import  NewsAdminPanel from '../../components/admin/NewsAdminPanel';
+
+const Noticias = () => {
+  return (
+    <div className="p-6">
+      <NewsAdminPanel />
+    </div>
+  );
+};
+
+export default Noticias;
+ 
