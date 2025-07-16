7fe7a1ca17e7af17095083b27dff4b2079f62017
jacksonhehe <jackcano14@gmail.com>
2025-07-16 10:21:40 -0500

    Add src/adminPanel/pages/admin/Torneos.tsx 1752679301679

---
+++ src/adminPanel/pages/admin/Torneos.tsx
@@ -0,0 +1,12 @@
+import  TournamentsAdminPanel from '../../components/admin/TournamentsAdminPanel';
+
+const Torneos = () => {
+  return (
+    <div className="p-6">
+      <TournamentsAdminPanel />
+    </div>
+  );
+};
+
+export default Torneos;
+ 
